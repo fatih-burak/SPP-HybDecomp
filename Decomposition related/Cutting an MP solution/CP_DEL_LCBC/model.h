88945b62bf63651cafca894500a53a129836a094
fatih-burak <fatihbakcay@outlook.com>
2025-05-14 18:01:30 +0200

    Add files via upload

---
+++ Decomposition related/Cutting an MP solution/CP_DEL_LCBC/model.h
@@ -0,0 +1,14 @@
+#ifndef CPM_H
+#define CPM_H
+
+#include <iostream>
+#include "gurobi_c++.h"
+#include "helper_functions.h"
+#include "slave_functions.h"
+
+using namespace std;
+void MP_CP_DEL_LCBC(Instance& inst, Solution& sol);
+
+
+#endif 
+
