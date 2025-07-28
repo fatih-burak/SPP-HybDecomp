cba958cae75577c1fa7eec8e6c430c209a4b167f
fatih-burak <fatihbakcay@outlook.com>
2025-07-28 12:39:39 +0200

    Add files via upload

---
+++ Direct Models/CC_CP/slave_functions.h
@@ -0,0 +1,12 @@
+#ifndef SLAVE_FUNCTIONS_H 
+#define SLAVE_FUNCTIONS_H
+
+#include "helper_functions.h"
+#include "model.h"
+//#include "gurobi_c++.h"
+
+int slave(const vector<vector<int>>& items_slave, const vector<vector<int>>& bins, const int& H, vector<vector<int>>& current_sol, int type); //type ensures different behaviour for slave. 0: main slave, 1: mis, -1: preprocess slave
+void preprocess_slave(vector<vector<int>>& items_slave, vector<vector<int>>& bins);
+
+
+#endif
---
+++ Direct Models/CC_CP_LB/model.cpp
@@ -0,0 +1,119 @@
+#include "model.h"
+#include <random>
+
+
+void CC_CP(Instance& inst, Solution& sol) {
+	int start = getCPUTime();
+	// from CSP to BPP
+	vector<vector<int>> items2;
+	int n2 = 0;
+	for (int j = 0; j < inst.n; j++) {
+		for (int k = 0; k < inst.items[j][2]; k++) {
+			items2.push_back({ inst.items[j][0],inst.items[j][1],j });
+			cout << "item=" << n2 << ": " << inst.items[j][0] << ", " << inst.items[j][1] << ", " << j << endl;
+			n2++;
+		}
+	}
+	inst.items2 = items2;
+
+	// create a model
+	IloEnv env;
+	IloModel model(env);
+
+	// define IloIntExprArray to store the end points of interval variables
+	IloIntExprArray ends(env);
+	// declaration of the variables for the model
+	IloIntervalVarArray x(env, n2);
+	IloIntervalVarArray y(env, n2);
+	IloCumulFunctionExpr z(env);
+	IloCumulFunctionExpr q(env);
+
+	IloSolution startSol(env); // ---- WARM START ----
+	// initizalization of the variables for the model
+	for (int j = 0; j < n2; j++) {
+		x[j] = IloIntervalVar(env);
+		x[j].setStartMin(0);
+		x[j].setStartMax(inst.W - items2[j][0]);
+		x[j].setSizeMin(items2[j][0]);
+		x[j].setSizeMax(items2[j][0]);
+		z += IloPulse(x[j], items2[j][1]);
+
+		y[j] = IloIntervalVar(env);
+		y[j].setStartMin(0);;
+		ends.add(IloEndOf(y[j]));
+		y[j].setSizeMin(items2[j][1]);
+		y[j].setSizeMax(items2[j][1]);
+		q += IloPulse(y[j], items2[j][0]);
+		
+		// ---- WARM START ----
+		int p_warm = get<0>(sol.packing_locations[j]);
+		int q_warm = get<1>(sol.packing_locations[j]);
+		startSol.setStart(x[j], p_warm);
+		startSol.setStart(y[j], q_warm);
+	}
+	
+	// set the cumul constraint
+	model.add(q <= inst.W);
+
+	//Non-overlap constraints
+	for (int j = 0; j < n2; j++) {
+		for (int i = j + 1; i < n2; i++) {
+			IloOr OR_const(env);
+			OR_const.add(IloEndOf(x[i]) <= IloStartOf(x[j]));
+			OR_const.add(IloEndOf(x[j]) <= IloStartOf(x[i]));
+			OR_const.add(IloEndOf(y[i]) <= IloStartOf(y[j]));
+			OR_const.add(IloEndOf(y[j]) <= IloStartOf(y[i]));
+			model.add(OR_const);
+		}
+	}
+
+	// set the objective: minimize the makespan
+	IloIntVar obj(env, 0, IloIntMax); //you can set a LB on z
+	model.add(obj >= IloMax(ends));
+	model.add(obj >= z); // adding it makes the model faster
+	model.add(obj >= sol.iLB);
+
+	//minimize objective
+	IloObjective objective = IloMinimize(env, obj);
+	model.add(objective);
+
+	// change some settings
+	IloCP cp(model);
+
+	cp.setStartingPoint(startSol); // ---- WARM START ----
+
+	cp.setParameter(IloCP::Workers, 1);
+	cp.setParameter(IloCP::LogPeriod, 500000);
+	//cp.setParameter(IloCP::FailureDirectedSearch, IloCP::On);
+	cp.setParameter(IloCP::CumulFunctionInferenceLevel, IloCP::Extended);
+	cp.setParameter(IloCP::SearchType, IloCP::Restart);
+	cp.setParameter(IloCP::TimeLimit,inst.timeLimit);
+	//cp.setParameter(IloCP::LogVerbosity, IloCP::Quiet);
+	cp.propagate();
+	//SOLVE
+	cp.solve();
+	int status = cp.getStatus();
+	sol.LB = cp.getObjBound();
+	if (status == 2) { // optimal solution is found
+		sol.UB = cp.getObjValue();
+		cout << "Optimal found" << endl; sol.opt = 1;
+		sol.LB = sol.UB;
+		for (int j = 0; j < n2; j++) {
+			cout << "Rectangle-" << j << " of size=" << items2[j][0] << "x" << items2[j][1] << " x coordinate = " << cp.getStart(x[j]) << ",y coordinate = " << cp.getStart(y[j]) << endl;
+		}
+	}
+	else if (status == 0 || (status == 1 && (getCPUTime() - start + 2) > inst.timeLimit)) { //time limit reached
+		cout << "Time limit is reached." << endl;
+		if (status == 1) {
+			cout << "However, some feasible solutions are found (which are not proven optimal)." << endl;
+			sol.UB = cp.getObjValue();
+		}
+	}
+	else if (status == 3) { //model is infeasible
+		cout << "Model is infeasible." << endl;
+		sol.opt = -1;
+	}
+
+	cp.end(); env.end();
+	return;
+}
